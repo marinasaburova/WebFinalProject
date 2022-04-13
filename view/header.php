563f811e208568c75e313af0c28109344a0effc4
Marina Saburova <79031317+marinasaburova@users.noreply.github.com>
2022-04-13 17:02:40 -0400

    style changes

--- view/header.php
+++ view/header.php
@@ -10,6 +10,7 @@
 
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+  <script src="https://kit.fontawesome.com/17b17c0b96.js" crossorigin="anonymous"></script>
 
   <title>Shopping Site</title>
 </head>
@@ -17,8 +18,16 @@
 <body>
   <!-- Container -->
   <div class="container-fluid">
+
+    <!-- Mini Nav -->
+    <div class="container d-flex justify-content-end flex-wrap sticky-top" style="z-index: 1022; top:14px">
+      <ul class="nav">
+        <li class="nav-item"><a href="cart.php#view" class="nav-link link-dark px-2"><i class="fa-solid fa-cart-shopping"></i></a></li>
+        <li class="nav-item"><a href="account.php#view" class="nav-link link-dark px-2"><i class="fa-solid fa-circle-user"></i></a></li>
+      </ul>
+    </div>
     <!-- Header -->
-    <div class="px-4 py-5 my-5 text-center">
+    <div class="px-4 py-5 mb-5 mt-2 text-center">
       <h1 class="display-5 fw-bold">Title</h1>
       <div class="col-lg-6 mx-auto">
         <p class="lead mb-4">A quick slogan.</p>
--- view/navigation.php
+++ view/navigation.php
@@ -3,12 +3,12 @@
   <header class="d-flex justify-content-center py-3 bg-white">
     <ul class="nav nav-pills">
       <li class="nav-item">
-        <a href="#" class="nav-link active" aria-current="page">Home</a>
+        <a href="home.php" class="nav-link active" aria-current="page">All Items</a>
       </li>
-      <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
-      <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
-      <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-      <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+      <li class="nav-item"><a href="home.php" class="nav-link">Category 1</a></li>
+      <li class="nav-item"><a href="home.php" class="nav-link">Category 2</a></li>
+      <li class="nav-item"><a href="home.php" class="nav-link">Category 3</a></li>
+      <li class="nav-item"><a href="home.php" class="nav-link">Category 4</a></li>
     </ul>
   </header>
 </nav>
