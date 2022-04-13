387b595b83ec05002bca503f87bf921ab36f44ac
Marina Saburova <79031317+marinasaburova@users.noreply.github.com>
2022-04-13 16:43:42 -0400

    designed pages

---
+++ page/account.php
@@ -0,0 +1,115 @@
+<!-- Displays user's account info -->
+<!-- User needs to be logged in to see -->
+
+<!-- Show's users cart and lets them edit it -->
+
+<!-- Displays details for a specific product -->
+<!-- Use GET for this page, so that users can bookmark it -->
+
+<?php
+include '../view/header.php';
+include '../view/navigation.php';
+?>
+
+
+<div class="container">
+    <main>
+        <div class="py-5 text-center">
+            <h2>Welcome, Name</h2>
+            <p class="lead">Manage your account here.</p>
+        </div>
+
+        <div class="row g-5">
+            <!-- Personal Info -->
+            <div class="col-md-5 col-lg-4 order-md-last">
+                <div class="sticky-top" style="top:72px; z-index: 1018">
+
+                    <h4 class="d-flex justify-content-between align-items-center mb-3">
+                        <span class="text-primary">Your Info</span>
+                    </h4>
+                    <ul class="list-group mb-3">
+                        <li class="list-group-item d-flex justify-content-between lh-sm">
+                            <div>
+                                <h6 class="my-0">Product total</h6>
+                                <small class="text-muted">Brief description</small>
+                            </div>
+                            <span class="text-muted">$71</span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between lh-sm">
+                            <div>
+                                <h6 class="my-0">Estimated Tax</h6>
+                                <small class="text-muted">Brief description</small>
+                            </div>
+                            <span class="text-muted">$2.01</span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between lh-sm">
+                            <div>
+                                <h6 class="my-0">Shipping</h6>
+                                <small class="text-muted">Brief description</small>
+                            </div>
+                            <span class="text-muted">$5</span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between bg-light">
+                            <div class="text-success">
+                                <h6 class="my-0">Promo code</h6>
+                                <small>EXAMPLECODE</small>
+                            </div>
+                            <span class="text-success">−$5</span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between">
+                            <span>Total (USD)</span>
+                            <strong>$92</strong>
+                        </li>
+                    </ul>
+
+                    <form class="card p-2" action="checkout.php#view">
+                        <div class="input-group">
+                            <button class="w-100 btn btn-secondary" type="submit">Edit</button>
+
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <!-- ./summary -->
+
+            <!-- Cart -->
+            <div class="col-md-7 col-lg-8">
+                <h4 class="mb-3 text-primary">Your Order History</h4>
+                <table class="table">
+                    <tbody>
+                        <tr>
+                            <th scope="row">Order Number</td>
+                            <td>Date</td>
+                            <td>Items Ordered</td>
+                            <td>Order Total</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Order Number</td>
+                            <td>Date</td>
+                            <td>Items Ordered</td>
+                            <td>Order Total</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Order Number</td>
+                            <td>Date</td>
+                            <td>Items Ordered</td>
+                            <td>Order Total</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Order Number</td>
+                            <td>Date</td>
+                            <td>Items Ordered</td>
+                            <td>Order Total</td>
+                        </tr>
+                    </tbody>
+
+                </table>
+
+            </div>
+            <!-- ./cart -->
+        </div>
+    </main>
+
+    <?php
+    include '../view/footer.php';
+    ?>
--- view/header.php
+++ view/header.php
@@ -3,32 +3,25 @@
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <!-- Bootstrap CSS -->
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-      crossorigin="anonymous"
-    />
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <title>Shopping Site</title>
-  </head>
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
 
-  <body>
-    <!-- Container -->
-    <div class="container-fluid">
-      <!-- Header -->
-      <div class="px-4 py-5 my-5 text-center">
-        <h1 class="display-5 fw-bold">Title</h1>
-        <div class="col-lg-6 mx-auto">
-          <p class="lead mb-4">A quick slogan.</p>
-        </div>
+  <title>Shopping Site</title>
+</head>
+
+<body>
+  <!-- Container -->
+  <div class="container-fluid">
+    <!-- Header -->
+    <div class="px-4 py-5 my-5 text-center">
+      <h1 class="display-5 fw-bold">Title</h1>
+      <div class="col-lg-6 mx-auto">
+        <p class="lead mb-4">A quick slogan.</p>
       </div>
-      <!-- /.header -->
     </div>
-  </body>
-</html>
+    <!-- /.header -->
