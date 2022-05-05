93d5f37a74577847f16e0e2f54917c79174dba1a
kelseynyman <kelsey01nyman@gmail.com>
2022-05-04 20:54:58 -0400

    store

--- function/store-db.php
+++ function/store-db.php
@@ -222,16 +222,16 @@ function updateOrder($orderID, $status, $email, $shipStreet, $shipStreet2, $ship
 {
     global $db;
 
-    $query = 'UPDATE orders SET `status` = :status `email` = :email `shipStreet` = :shipStreet `shipStreet2` = :shipStreet2 `shipCity` = :shipCity `shipState` = :shipState `shipZip` = :shipZip  WHERE `orderID` = :orderID';
+    $query = 'UPDATE orders SET `status` = :status, `email` = :email, `shipStreet` = :shipStreet, `shipStreet2` = :shipStreet2, `shipCity` = :shipCity, `shipState` = :shipState, `shipZip` = :shipZip  WHERE `orderID` = :orderID';
     $statement = $db->prepare($query);
     $statement->bindValue(':orderID', $orderID);
     $statement->bindValue(':status', $status);
-    $statement->bindValue('email', $email);
-    $statement->bindValue('shipStreet', $shipStreet);
-    $statement->bindValue('shipStreet2', $shipStreet2);
-    $statement->bindValue('shipCity', $shipCity);
-    $statement->bindValue('shipState', $shipState);
-    $statement->bindValue('shipZip', $shipZip);
+    $statement->bindValue(':email', $email);
+    $statement->bindValue(':shipStreet', $shipStreet);
+    $statement->bindValue(':shipStreet2', $shipStreet2);
+    $statement->bindValue(':shipCity', $shipCity);
+    $statement->bindValue(':shipState', $shipState);
+    $statement->bindValue(':shipZip', $shipZip);
     $statement->execute();
     $statement->closeCursor();
 }
