5f0516f1841402b97fc3e7efc300956bc917a2d5
Marina Saburova <79031317+marinasaburova@users.noreply.github.com>
2022-05-06 08:08:38 -0400

    updated header

--- admin/admin-view/header.php
+++ admin/admin-view/header.php
@@ -12,7 +12,7 @@
   <link href="../bootstrap/css/main.min.css" rel="stylesheet" />
   <script src="https://kit.fontawesome.com/17b17c0b96.js" crossorigin="anonymous"></script>
 
-  <title>Admin</title>
+  <title>Kelarina Admin</title>
 </head>
 
 <body>
@@ -31,7 +31,7 @@
     </div>
     <!-- Header -->
     <div class="px-4 py-2 mb-5 mt-2 text-center position-relative">
-      <h1 class="display-5 fw-bold">Hello, name</h1>
+      <h1 class="display-5 fw-bold">Kelarina Admin</h1>
       <div class="col-lg-6 mx-auto">
         <p class="lead mb-4">Manage the store.</p>
       </div>
